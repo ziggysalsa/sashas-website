dd585f7cc059618380c0ab45ec31f2b5a43ffe72
ziggysalsa <zelenski.sasha@gmail.com>
2025-04-13 22:13:46 -0700

    initial website

---
+++ src/consts.ts
@@ -0,0 +1,17 @@
+export const SITE_TITLE = "Sasha's Stuff";
+export const SITE_DESCRIPTION = "Placeholder site description";
+export const COPYRIGHT = "© 2025 Sasha Zelenski";
+
+export const SOCIAL_LINKS = {
+  Github: "https://github.com/ziggysalsa",
+  LinkedIn: "https://www.linkedin.com/in/yourusername",
+};
+
+export const NAV_ITEMS = [
+  { text: "Home", link: "/" },
+  //{ text: "About", link: "/about" },
+  { text: "Resume", link: "/resume" },
+  //{ text: "Blog", link: "/blog" },
+  { text: "Projects", link: "/projects" },
+  // { text: "Tags", link: "/tags" },
+];
